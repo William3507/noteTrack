36a8df4f32ff201efdd2bfcb7ba88e5ab16f3066
William Bailey <wbaileyfletcher@gmail.com>
2025-09-23 11:23:00 -0400

    Initial commit - Xcode blank Swift template

---
+++ NoteUpLoader/NoteUpLoader/NoteUpLoaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteUpLoaderApp.swift
+//  NoteUpLoader
+//
+//  Created by William Bailey on 9/23/25.
+//
+
+import SwiftUI
+
+@main
+struct NoteUpLoaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NoteUpLoader/NoteUpLoaderTests/NoteUpLoaderTests.swift
@@ -0,0 +1,17 @@
+//
+//  NoteUpLoaderTests.swift
+//  NoteUpLoaderTests
+//
+//  Created by William Bailey on 9/23/25.
+//
+
+import Testing
+@testable import NoteUpLoader
+
+struct NoteUpLoaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
